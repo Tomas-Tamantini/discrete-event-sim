c2bc23c8371a46e68700cd1ff9c92780be016871
Tomas Tamantini <tomasbft@gmail.com>
2022-03-24 14:06:37 -0300

    Implement pop_event from calendar

--- models/event_simulation/event_calendar.py
+++ models/event_simulation/event_calendar.py
@@ -8,6 +8,14 @@ class EventCalendar:
     def schedule(self, event: Event) -> None:
         self.__scheduled_events.append(event)
 
+    def pop_event(self) -> Event:
+        if self.is_empty:
+            raise self.UnderflowError('Cannot pop from empty calendar')
+        return self.__scheduled_events.pop(0)
+
     @property
     def is_empty(self) -> bool:
         return len(self.__scheduled_events) == 0
+
+    class UnderflowError(Exception):
+        pass
--- tests/test_event_calendar.py
+++ tests/test_event_calendar.py
@@ -1,11 +1,16 @@
-from dataclasses import dataclass
-
+import pytest
 from models import Event, EventCalendar
 
 
 def test_calendar_starts_out_empty():
     calendar = EventCalendar()
     assert calendar.is_empty
+
+
+def test_pop_with_empty_calendar_is_underflow():
+    calendar = EventCalendar()
+    with pytest.raises(EventCalendar.UnderflowError):
+        _ = calendar.pop_event()
 
 
 def test_after_push_calendar_is_not_empty():
