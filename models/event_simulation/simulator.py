adbb8810a5f5e8ea2e80ca95e20289168b40f4d7
Tomas Tamantini <tomasbft@gmail.com>
2022-03-24 15:32:26 -0300

    Start implementing main simulation method

--- models/event_simulation/simulator.py
+++ models/event_simulation/simulator.py
@@ -1,11 +1,20 @@
+from typing import Optional
+
 from .model import Model
 
 
 class DiscreteEventSimulator:
     def __init__(self, model: Model) -> None:
         self.__model = model
+        self.__calendar = model.initial_calendar()
         self.__current_time = 0.0
 
     @property
     def current_time(self) -> float:
         return self.__current_time
+
+    def run(self, time_horizon: float = float('inf')) -> None:
+        while not self.__calendar.is_empty and self.__calendar.time_of_next_event <= time_horizon:
+            next_event = self.__calendar.pop_event()
+            next_event.fire()
+            self.__current_time = next_event.scheduled_time
--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -1,10 +1,35 @@
-from models import DiscreteEventSimulator
+from models import DiscreteEventSimulator, Event, EventCalendar
+from models.event_simulation import event
+
+
+class EventSpy(Event):
+    def __init__(self, scheduled_time: float = 0) -> None:
+        super().__init__(scheduled_time)
+        self.has_been_called = False
+
+    def fire(self) -> None:
+        self.has_been_called = True
 
 
 class ModelStub:
-    pass
+    def __init__(self, events_in_inital_calendar: list[EventSpy] = None) -> None:
+        self.__initial_calendar = EventCalendar()
+        if events_in_inital_calendar:
+            for e in events_in_inital_calendar:
+                self.__initial_calendar.schedule(e)
+
+    def initial_calendar(self) -> EventCalendar:
+        return self.__initial_calendar
 
 
 def test_simulator_current_time_starts_at_zero():
     simulator = DiscreteEventSimulator(ModelStub())
     assert simulator.current_time == 0
+
+
+def test_simulator_fires_initial_events():
+    event = EventSpy(scheduled_time=10.0)
+    simulator = DiscreteEventSimulator(ModelStub([event]))
+    simulator.run()
+    assert simulator.current_time == 10.0
+    assert event.has_been_called
