1708c4a53bfbceb892af872939709dda201eb572
Tomas Tamantini <tomasbft@gmail.com>
2022-03-24 15:31:16 -0300

    Add fire() method to event

--- models/event_simulation/event.py
+++ models/event_simulation/event.py
@@ -1,6 +1,16 @@
+from typing import Iterator, Optional
+
+
 class Event:
     def __init__(self, scheduled_time: float = 0.0) -> None:
         self.__scheduled_time = scheduled_time
 
+    @property
+    def scheduled_time(self) -> float:
+        return self.__scheduled_time
+
     def __lt__(self, other: 'Event') -> bool:
         return self.__scheduled_time < other.__scheduled_time
+
+    def fire(self) -> Optional[Iterator["Event"]]:
+        raise NotImplementedError('Subclasses must implement this method')
