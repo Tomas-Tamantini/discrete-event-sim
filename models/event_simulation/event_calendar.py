edc689c2b3f914475d86674c5b6174863bd7378f
Tomas Tamantini <tomasbft@gmail.com>
2022-03-24 14:03:02 -0300

    Push events to calendar

--- models/event_simulation/event_calendar.py
+++ models/event_simulation/event_calendar.py
@@ -1,4 +1,13 @@
+from .event import Event
+
+
 class EventCalendar:
+    def __init__(self):
+        self.__scheduled_events = []
+
+    def schedule(self, event: Event) -> None:
+        self.__scheduled_events.append(event)
+
     @property
     def is_empty(self) -> bool:
-        return True
+        return len(self.__scheduled_events) == 0
--- tests/test_event_calendar.py
+++ tests/test_event_calendar.py
@@ -1,6 +1,15 @@
-from models import EventCalendar
+from dataclasses import dataclass
+
+from models import Event, EventCalendar
 
 
 def test_calendar_starts_out_empty():
     calendar = EventCalendar()
     assert calendar.is_empty
+
+
+def test_after_push_calendar_is_not_empty():
+    calendar = EventCalendar()
+    event = Event()
+    calendar.schedule(event)
+    assert not calendar.is_empty
