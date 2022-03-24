9ce88cb87f5a1b358e1201bd65eb20057b374092
Tomas Tamantini <tomasbft@gmail.com>
2022-03-24 14:13:46 -0300

    Add event time_to_fire and make it sortable

--- models/event_simulation/event.py
+++ models/event_simulation/event.py
@@ -1,2 +1,6 @@
 class Event:
-    pass
+    def __init__(self, time_to_fire: float = 0.0) -> None:
+        self.__time_to_fire = time_to_fire
+
+    def __lt__(self, other: 'Event') -> bool:
+        return self.__time_to_fire < other.__time_to_fire
