eb4084f84beaa290149c0d4e81035dd12ea62334
Tomas Tamantini <tomasbft@gmail.com>
2022-03-25 11:49:06 -0300

    Add str method to Train class

--- models/train/train.py
+++ models/train/train.py
@@ -11,3 +11,14 @@ class Train:
     is_loaded: bool
     status: TrainStatus
     flow_destination: Optional[Hashable] = None
+
+    def __str__(self) -> str:
+        s = f'Train {self.train_id} ({"loaded" if self.is_loaded else "empty"}) at {self.location} '
+        if self.status == TrainStatus.EN_ROUTE:
+            if self.is_loaded:
+                status_str = f'en route to {self.flow_destination}'
+            else:
+                status_str = f'en route from {self.flow_destination}'
+        else:
+            status_str = self.status.value.lower().replace('_', ' ')
+        return s + status_str
